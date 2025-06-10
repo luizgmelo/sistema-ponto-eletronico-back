84892b768b1bb2498d3341798f7cf9ea270b45c3
Luiz Guilherme <luizgmelo64@gmail.com>
2025-06-10 20:07:23 -0300

    Initial configuration

---
+++ src/main/java/com/luizgmelo/sistema_ponto_eletronico/SistemaPontoEletronicoApplication.java
@@ -0,0 +1,14 @@
+package com.luizgmelo.sistema_ponto_eletronico;
+
+import com.luizgmelo.sistema_ponto_eletronico.enums.UserRole;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPontoEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPontoEletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizgmelo/sistema_ponto_eletronico/SistemaPontoEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizgmelo.sistema_ponto_eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPontoEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
