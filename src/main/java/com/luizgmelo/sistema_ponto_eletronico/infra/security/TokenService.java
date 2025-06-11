7e1df0e07607e9cc394079107ea1f8644ab9c36c
Luiz Guilherme <luizgmelo64@gmail.com>
2025-06-10 22:52:18 -0300

    fix: token subject is cpf

--- src/main/java/com/luizgmelo/sistema_ponto_eletronico/infra/security/TokenService.java
+++ src/main/java/com/luizgmelo/sistema_ponto_eletronico/infra/security/TokenService.java
@@ -22,7 +22,7 @@ public class TokenService {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("sistema-ponto-eletronico")
-                    .withSubject(user.getName())
+                    .withSubject(user.getCpf())
                     .withExpiresAt(this.generateExpirationDate())
                     .sign(algorithm);
         } catch (JWTCreationException e) {
